65ca759f62f62db105325eb058a5c4b456b43e63
KRolander <kromesrolandgyula@gmail.com>
2024-11-13 15:06:58 +0100

    New features UpdatePOS(): identify infra conflicts and creates a first allocation for movment. TODO: add revisiting feature when all moves were verified, and link movments

--- ServiceSiteScheduling/Solutions/PartialOrderSchedule.cs
+++ ServiceSiteScheduling/Solutions/PartialOrderSchedule.cs
@@ -12,6 +12,9 @@ using Google.Protobuf;
 using System.ComponentModel.DataAnnotations;
 using System.Transactions;
 using System.Runtime.CompilerServices;
+using ServiceSiteScheduling.Servicing;
+using System.Xml;
+
 
 namespace ServiceSiteScheduling.Solutions
 {
@@ -29,6 +32,7 @@ namespace ServiceSiteScheduling.Solutions
 
         public List<MoveTask> ListOfMoves { get; set; }
 
+        public Dictionary<ulong, Infrastructure> DictOfInrastructure { get; set; }
         public PartialOrderSchedule(MoveTask first, ShuntTrainUnit[] shuntunits, ArrivalTask[] arrivals, DepartureTask[] departures)
         {
             this.First = first;
@@ -37,9 +41,193 @@ namespace ServiceSiteScheduling.Solutions
             this.DepartureTasks = departures;
         }
 
-        public void UpdatePOS(MoveTask first)
+        public Dictionary<ulong, Infrastructure> GetInfrasturcture()
+        {
+            ProblemInstance instance = ProblemInstance.Current;
+
+            Dictionary<ulong, Infrastructure> infrastructuremap = new Dictionary<ulong, Infrastructure>();
+            int index = 0;
+
+            foreach (var part in instance.InterfaceLocation.TrackParts)
+            {
+                switch (part.Type)
+                {
+                    case AlgoIface.TrackPartType.RailRoad:
+                        Track track = new Track(part.Id, part.Name, ServiceType.None, (int)part.Length, Side.None, part.ParkingAllowed, part.SawMovementAllowed);
+                        track.Index = index++;
+                        // tracks.Add(track);
+                        infrastructuremap[part.Id] = track;
+                        break;
+                    case AlgoIface.TrackPartType.Switch:
+                        infrastructuremap[part.Id] = new Switch(part.Id, part.Name);
+                        break;
+                    case AlgoIface.TrackPartType.EnglishSwitch:
+                        infrastructuremap[part.Id] = new EnglishSwitch(part.Id, part.Name);
+                        break;
+                    case AlgoIface.TrackPartType.HalfEnglishSwitch:
+                        infrastructuremap[part.Id] = new HalfEnglishSwitch(part.Id, part.Name);
+                        break;
+                    case AlgoIface.TrackPartType.Intersection:
+                        infrastructuremap[part.Id] = new Intersection(part.Id, part.Name);
+                        break;
+                    case AlgoIface.TrackPartType.Bumper:
+                        var gateway = new GateWay(part.Id, part.Name);
+                        infrastructuremap[part.Id] = gateway;
+                        // gateways.Add(gateway);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return infrastructuremap;
+
+        }
+
+        // Returns list of the IDs of the Infrastructure used by a movement
+        public List<ulong> GetIDListOfInfraUsed(MoveTask move)
+        {
+            List<ulong> IDListOfInstraUsed = new List<ulong>();
+
+            var routing = move as RoutingTask;
+
+            if (routing != null)
+            {
+                var tracks = routing.Route.Tracks;
+                var lastTrack = tracks.Last();
+
+                foreach (Track track in tracks)
+                {
+
+                    IDListOfInstraUsed.Add(track.ASide.ID);
+                    IDListOfInstraUsed.Add(track.ID);
+                    IDListOfInstraUsed.Add(track.BSide.ID);
+
+                }
+            }
+
+            return IDListOfInstraUsed;
+        }
+
+
+        public bool InfraConflict(Dictionary<Infrastructure, int> InfraOccupiedByMovesID, List<ulong> IDListOfInfraUsed, int moveID, ref int newMoveId, bool revisit)
         {
 
+            Dictionary<ulong, Infrastructure> DictOfInfrastrucure = this.DictOfInrastructure;
+
+            bool ConflictOccured = false;
+
+            foreach (ulong id in IDListOfInfraUsed)
+            {
+                if (InfraOccupiedByMovesID[DictOfInfrastrucure[id]] != moveID && InfraOccupiedByMovesID[DictOfInfrastrucure[id]] == 999)
+                {
+                    ConflictOccured = false;
+                    newMoveId = InfraOccupiedByMovesID[DictOfInfrastrucure[id]];
+                }
+                else if (InfraOccupiedByMovesID[DictOfInfrastrucure[id]] != moveID && InfraOccupiedByMovesID[DictOfInfrastrucure[id]] != 999 && revisit == true)
+                {
+                    ConflictOccured = true;
+                    newMoveId = InfraOccupiedByMovesID[DictOfInfrastrucure[id]];
+                }
+                else
+                {
+                    newMoveId = InfraOccupiedByMovesID[DictOfInfrastrucure[id]];
+                    return true;
+                }
+            }
+
+
+            return ConflictOccured;
+        }
+
+
+        public void UpdatePOS()
+        {
+
+            List<MoveTask> listOfMoves = this.ListOfMoves;
+            Dictionary<Infrastructure, MoveTask> InfraOccupiedByMoves = new Dictionary<Infrastructure, MoveTask>();
+
+            Dictionary<Infrastructure, int> InfraOccupiedByMovesID = new Dictionary<Infrastructure, int>();
+
+            Dictionary<ulong, Infrastructure> DictOfInfrastrucure = this.DictOfInrastructure;
+
+            // Init dictionary for infrastructures occupied by moves
+
+            bool Test = true;
+
+            foreach (KeyValuePair<ulong, Infrastructure> infra in DictOfInfrastrucure)
+            {
+                InfraOccupiedByMoves[infra.Value] = null;
+                InfraOccupiedByMovesID[infra.Value] = 999;
+            }
+
+
+            int ok = 1;
+            int moveIndex = 0;
+            int newMoveId = 0;
+            bool revisit = false;
+
+            while (ok != 0)
+            {
+                var currentMove = listOfMoves[moveIndex];
+                List<ulong> IDListOfInfraUsed = GetIDListOfInfraUsed(currentMove); // used by the movement
+
+
+
+                if ((InfraConflict(InfraOccupiedByMovesID, IDListOfInfraUsed, moveIndex, ref newMoveId , revisit) != true) && (revisit == false))
+                {
+                    // No conflict occured
+                    foreach (ulong infraID in IDListOfInfraUsed)
+                    {
+
+                        InfraOccupiedByMoves[DictOfInfrastrucure[infraID]] = currentMove;
+                        InfraOccupiedByMovesID[DictOfInfrastrucure[infraID]] = moveIndex;
+                    }
+                }
+                else if((InfraConflict(InfraOccupiedByMovesID, IDListOfInfraUsed, moveIndex, ref newMoveId , revisit) == true) && (revisit == true))
+                {
+                    // Conflict occured
+                    // TODO: Replace the newMoveId with in the list by 999 and do the same steps like in no conflict occured step
+                   //  Console.WriteLine($" ****** ******* ***** **** **** Conflicting ID: {newMoveId}  ****** ******* ***** **** ****");
+                }
+                else if((InfraConflict(InfraOccupiedByMovesID, IDListOfInfraUsed, moveIndex, ref newMoveId , revisit) == true))
+                {
+                    Console.WriteLine($" ****** ******* ***** **** **** Conflicting ID: {newMoveId} with move {moveIndex}  ****** ******* ***** **** ****");
+
+                }
+     
+
+                moveIndex++;
+
+                if (Test)
+                {
+                    if (moveIndex > 2)
+                        ok = 0;
+                }
+            }
+
+            if (Test)
+            {
+                Console.WriteLine("----------------------------------");
+
+                Console.WriteLine("------ Infrastructure - Move -----");
+
+                Console.WriteLine("----------------------------------");
+
+                foreach (KeyValuePair<Infrastructure, int> pairInraMove in InfraOccupiedByMovesID)
+                {
+                    Console.WriteLine($"Inrastrucure : {pairInraMove.Key} - {pairInraMove.Value}");
+                }
+
+                //  A side 42 Sein70 --> 25 Spoor906a (0 B) -->  B side 58 Wissel963 --> A side 57 Wissel961 --> 39 Spoor52 (473 Both) --> B side 70 Engels974975 
+
+
+            }
+        }
+
+
+        public void CreatePOS(MoveTask first)
+        {
             MoveTask move = this.First;
 
             List<MoveTask> listOfMoves = new List<MoveTask>();
@@ -54,11 +242,33 @@ namespace ServiceSiteScheduling.Solutions
                 move = move.NextMove;
             }
             this.ListOfMoves = listOfMoves;
-
             this.First = listOfMoves.First();
+
+            this.DictOfInrastructure = GetInfrasturcture();
+
         }
         public void DisplayInfrastructure()
         {
+            Console.WriteLine("-------------------------------");
+
+            Console.WriteLine("------Main Infrastructure------");
+
+            Console.WriteLine("-------------------------------");
+
+            Dictionary<ulong, Infrastructure> infrastructuremap = GetInfrasturcture();
+
+            foreach (KeyValuePair<ulong, Infrastructure> infra in infrastructuremap)
+            {
+                Console.WriteLine($"id: {infra.Key} : Inrastructure {infra.Value}");
+
+            }
+
+
+            Console.WriteLine("-------------------------------");
+
+
+
+
             MoveTask move = this.First;
             int i = 0;
             while (move != null)
