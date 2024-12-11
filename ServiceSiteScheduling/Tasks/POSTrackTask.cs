edb1ee7330482b961da8914eb4a3e9038926dacb
KRolander <kromesrolandgyula@gmail.com>
2024-12-11 14:46:28 +0100

    Add new constructor to POSTrackTask and ToString() function

--- ServiceSiteScheduling/Tasks/POSTrackTask.cs
+++ ServiceSiteScheduling/Tasks/POSTrackTask.cs
@@ -7,30 +7,132 @@ using System.Threading.Tasks;
 
 namespace ServiceSiteScheduling.Tasks
 {
-    public enum POSTrackTaskType { Arrival, Departure, Service, Parking, Split, Combine}
+    public enum POSTrackTaskType { Arrival, Departure, Service, Parking, Split, Combine }
 
     class POSTrackTask
     {
-        public TrackTask CorrespondingTrackTask {get; }
+        public TrackTask CorrespondingTrackTask { get; set; }
 
-        public int ID {get; set;}
+        public int ID { get; set; }
 
         // public POSTrackTask nextTasks {get; set;}
 
         public List<POSMoveTask> previousMoves { get; set; }
 
         public List<POSMoveTask> nextMoves { get; set; }
-        
-        public POSTrackTaskType TaskType { get { return this.tasktype; } }
 
-        protected readonly POSTrackTaskType tasktype;
+        public POSTrackTaskType TaskType { get; set; }
+
+        public Trains.ShuntTrain Train { get; set; }
+
+        public TrackParts.Track Track { get; set; }
 
 
-        public POSTrackTask(POSTrackTaskType type, TrackTask correspondingTrackTask)
+
+
+        public POSTrackTask(TrackTask correspondingTrackTask)
         {
-    
             this.CorrespondingTrackTask = correspondingTrackTask;
-            this.tasktype = type;
+
+
+            switch (CorrespondingTrackTask.TaskType)
+            {
+                case TrackTaskType.Arrival:
+                    this.TaskType = POSTrackTaskType.Arrival;
+                    break;
+
+                case TrackTaskType.Departure:
+                    this.TaskType = POSTrackTaskType.Departure;
+                    break;
+
+                case TrackTaskType.Parking:
+                    this.TaskType = POSTrackTaskType.Parking;
+                    break;
+
+                case TrackTaskType.Service:
+                    this.TaskType = POSTrackTaskType.Service;
+                    break;
+
+                // Default value
+                default:
+                    this.TaskType = POSTrackTaskType.Service;
+                    break;
+            }
+
+            this.previousMoves = new List<POSMoveTask>();
+
+            this.nextMoves = new List<POSMoveTask>();
+
+            this.Train = correspondingTrackTask.Train;
+
+            this.Track = correspondingTrackTask.Track;
+
+        }
+
+        public void setPOSTrackTaskType(POSTrackTaskType POSTrackTaskType)
+        {
+
+            this.TaskType = POSTrackTaskType;
+        }
+
+        public override string ToString()
+        {
+
+            string POStype = "";
+
+            switch (this.TaskType)
+            {
+                case POSTrackTaskType.Arrival:
+                    POStype = "Arrival";
+                    break;
+
+                case POSTrackTaskType.Departure:
+                    POStype = "Departure";
+                    break;
+
+                case POSTrackTaskType.Parking:
+                    POStype = "Parking";
+                    break;
+
+                case POSTrackTaskType.Service:
+                    POStype = "Service";
+                    break;
+
+                case POSTrackTaskType.Split:
+                    POStype = "Split";
+                    break;
+
+                 case POSTrackTaskType.Combine:
+                    POStype = "Combine";
+                    break;
+                // Default value
+                default:
+                    POStype = "Service";
+                    break;
+            }
+
+
+
+
+            string str = "POSTrackTask " + this.ID + " - " + POStype + "Train: " + Train + " at " + Track.ID + ":\n";
+            str = str + "|POSMoveTask Successors: [";
+
+            foreach (POSMoveTask successor in nextMoves)
+            {
+                str = str + "Move " + successor.ID + " , ";
+            }
+
+            str = str + "]\n|POSMoveTask Predeccessors|: [";
+
+            foreach (POSMoveTask predeccessor in previousMoves)
+            {
+                str = str + "Move " + predeccessor.ID + ", ";
+
+            }
+            str = str + "]\n";
+
+
+            return str;
         }
 
     }
